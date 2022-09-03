d74f456d45b75719022f78ca6a8340f1bafc9146
iamjosephlegazzi <jcharles.wemail@gmail.com>
2022-09-02 21:24:31 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewToThis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HiImNewToCoding
+            Console.WriteLine("Hi Im New To Coding");
+
+            Console.ReadKey();
+        }
+    }
+}
